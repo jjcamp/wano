e5fe15ba7b3261d8c27fc09dc36d456a8451fc85
John Camp <jjcamp@gmail.com>
2016-11-12 15:10:59 -0500

    Updated wano.h to use new ui header.

--- wano/wano.h
+++ wano/wano.h
@@ -2,8 +2,7 @@
 
 #include <stdio.h>
 
-#include "../curses_ui/curses_obj.h"
-#include "../curses_ui/getmodch.h"
+#include "../curses_ui/curses_ui.h"
 #include "menu.h"
 #include "textarea.h"
 #include "events.h"
